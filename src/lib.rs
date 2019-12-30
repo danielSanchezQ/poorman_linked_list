df32502613fe22564bd1fc9bdcd85284e87f0718
danielsanchezq <danielsanchez@off-the-grid.io>
2019-12-30 17:53:22 +0100

    Implemented as a single linked list Implemented iterator

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,18 @@
-use std::rc::Rc;
-use std::ops::{Index, Deref};
-use std::process::Output;
-use std::borrow::{Borrow, BorrowMut};
-use std::cell::RefCell;
+use std::ops::{Index};
 
 #[derive(Debug)]
 struct Node<T> {
-    prev :  Option<Rc<RefCell<Node<T>>>>,
-    next :  Option<Rc<RefCell<Node<T>>>>,
+    next :  Option<Box<Node<T>>>,
     value : Box<T>,
 }
 
 impl<T> Node<T> {
+    pub fn new(val : T, next : Option<Box<Node<T>>>) -> Self {
+        Node {
+            next,
+            value : Box::new(val)
+        }
+    }
     pub fn get(&self) -> &T {
         self.value.as_ref()
     }
@@ -19,8 +20,7 @@ impl<T> Node<T> {
 
 #[derive(Debug)]
 struct LinkedList<T> {
-    first : Option<Rc<RefCell<Node<T>>>>,
-    last  : Option<Rc<RefCell<Node<T>>>>,
+    first : Option<Box<Node<T>>>,
     len : usize,
 }
 
@@ -29,7 +29,6 @@ impl<T> LinkedList<T> {
     fn new() -> Self {
         LinkedList{
             first: None,
-            last : None,
             len: 0
         }
     }
@@ -43,110 +42,73 @@ impl<T> LinkedList<T> {
     }
 
     fn append(&mut self, val : T) {
-        match self.last {
+        match self.first.as_mut() {
             None => {
-                let node : Rc<RefCell<Node<T>>>= Rc::new(RefCell::new(
-                    Node {
-                    prev : None,
-                    next : None,
-                    value: Box::new(val),
-                }));
-                self.first = Some(node.clone());
-                self.last = Some(node);
-            }
-            Some(ref last_node) => {
-                let node : Rc<RefCell<Node<T>>> = Rc::new(RefCell::new(
-                    Node {
-                    prev : Some(last_node.clone()),
-                    next : None,
-                    value: Box::new(val),
-                }));
-                let ln : &RefCell<Node<T>> = last_node.borrow();
-                ln.borrow_mut().next = Some(node.clone());
-                self.last = Some(node);
+                self.first = Some(Box::new(Node::new(val, None)));
+            },
+            _ => {
+                let mut node = self.first.as_mut().unwrap();
+                for _ in 0..self.len-1 {
+                    node = node.next.as_mut().unwrap();
+                }
+                node.next = Some(Box::new(Node::new(val, None)));
             }
         }
         self.len += 1;
     }
 
-//    fn get(&self, i : usize) -> &T {
-//        if i >= self.len() {
-//            panic!()
-//        }
-//        let node : &RefCell<Node<T>> = self.last.as_ref().unwrap().borrow();
-//        node.into_inner().get()
-////        self.last.as_ref().unwrap()().get()
-////        let mut node = self.first.as_ref().unwrap();
-////        for _ in 0..i {
-//////            node = &node.next.as_ref().unwrap();
-////            node = &node.get().next.unwrap();
-////        }
-////        node.as_ref().borrow().value.clone()
-//    }
-}
-
-impl<T> Index<usize> for LinkedList<T> where T : Clone {
-    type Output = T;
-    fn index<'a>(&'a self, i: usize) -> &'a T {
+    fn get(&self, i : usize) -> &T {
         if i >= self.len() {
             panic!()
         }
-        self.first.as_ref().unwrap().into_inner().get()
-//        let mut node = self.first.as_ref().unwrap();
-
-//        for _ in 0..i {
-////            node = &node.next.as_ref().unwrap();
-//            node = &*node.borrow().borrow().next.unwrap();
-//        }
-//        &*node.as_ref().borrow().value
+        let mut node = self.first.as_ref().unwrap();
+        for _ in 0..i {
+            node = node.next.as_ref().unwrap()
+        }
+        node.get()
     }
-//    fn index<'a>(&'a self, i: usize) -> &'a Self::Output {
-//        if i >= self.len() {
-//            panic!()
-//        }
-//        let half = self.len()/2;
-//        let mut node : Rc<RefCell<Node<T>>> = match i {
-//            i if i <= half => {
-//                self.first.as_ref().unwrap().clone()
-//            }
-//            _ => {
-//                self.last.as_ref().unwrap().clone()
-//            }
-//        };
-////        let next_f : fn(&Rc<RefCell<Node<T>>>) -> &Rc<RefCell<Node<T>>> = match i {
-////            i if i <= half => {
-////                | node : &Rc<RefCell<Node<T>>>| {
-////                    let n : &RefCell<Node<T>> = node.borrow();
-////                    n.borrow().next.as_ref().unwrap()
-////                }
-////            }
-////            _ => {
-////                | node : &Rc<RefCell<Node<T>>>| {
-////                    let n : &RefCell<Node<T>> = node.borrow();
-////                    n.borrow().prev.as_ref().unwrap()
-////                }
-////            }
-////        };
-//        let rng = match i {
-//            i if i <= half => {
-//                0..i
-//            }
-//            i => {
-//                0..(self.len()-i)
-//            }
-//        };
-//        for _ in rng {
-//            node = match i {
-//                i if i <= half => {
-//                    *node.as_ref().borrow().next.as_ref().unwrap()
-//                }
-//                _ => {
-//                    *node.as_ref().borrow().prev.as_ref().unwrap()
-//                }
-//            }
-//        }
-//        &node.as_ref().borrow().value
-//    }
+}
+
+impl<T> Index<usize> for LinkedList<T> {
+    type Output = T;
+    fn index(&self, i: usize) -> &T {
+        self.get(i)
+    }
+}
+
+struct LLIter<'a, T: 'a> {
+    ll : &'a LinkedList<T>,
+    index : usize,
+}
+
+impl<'a, T> Iterator for LLIter<'a, T> {
+    // we will be counting with usize
+    type Item = &'a T;
+
+    // next() is the only required method
+    fn next(&mut self) -> Option<&'a T> {
+        // Increment our count. This is why we started at zero.
+        self.index += 1;
+
+        // Check to see if we've finished counting or not.
+        if self.index <= self.ll.len() {
+            Some(self.ll.get(self.index-1))
+        } else {
+            None
+        }
+    }
+}
+
+impl<'a, T> IntoIterator for &'a LinkedList<T> {
+    type Item = &'a T;
+    type IntoIter = LLIter<'a, T>;
+
+    fn into_iter(mut self) -> LLIter<'a, T> {
+        LLIter{
+            ll  : self,
+            index : 0,
+        }
+    }
 }
 
 
@@ -170,13 +132,16 @@ mod tests {
         assert_eq!(ll.len(), 10);
     }
 
-//    #[test]
-//    fn access() {
-//        let mut ll:LinkedList<i32> = LinkedList::new();
-//        for i in 0..10 {
-//            ll.append(i);
-//        }
-//        assert_eq!(ll.len(), 10);
-//        assert_eq!(ll[0], 10);
-//    }
+    #[test]
+    fn access() {
+        let mut ll:LinkedList<usize> = LinkedList::new();
+        for i in 0..10 as usize {
+            ll.append(i);
+            assert_eq!(ll[i], i);
+        }
+        assert_eq!(ll.len(), 10);
+        for (i, e) in (0..10).zip(ll.into_iter()) {
+            assert_eq!(i as usize, *e);
+        }
+    }
 }
